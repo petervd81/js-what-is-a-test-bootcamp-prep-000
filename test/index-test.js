27f38256332c427223e66a17ca9766114a12d1e4
Peter van Dijk <peter@petervandijk.net>
2018-05-19 10:07:19 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,6 +1,6 @@
 var index = require("../index.js")
 
-
+/*
 describe('what-is-a-test', () => {
   describe('Name', () => {
     it('returns "Susan"', () => {
@@ -8,7 +8,6 @@ describe('what-is-a-test', () => {
     })
   })
 
-/*
   describe('Height', () => {
     it('is less than 40', () => {
       expect(index.height).toBeLessThan(40)
